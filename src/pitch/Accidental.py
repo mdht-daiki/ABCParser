9334b483e83937cca3a3c6bf3b572fc3c12eab6e
mdht-daiki <70727838+mdht-daiki@users.noreply.github.com>
2022-08-27 10:58:22 +0900

    [add]Key.py
    Key.pyを追加し、pitchモジュールを整理

--- src/pitch/Accidental.py
+++ src/pitch/Accidental.py
@@ -1,20 +1,9 @@
 from enum import Enum, auto
 
 
-key_accidental_order = ["F", "C", "G", "D", "A", "E", "B"]
-
-
 class Accidental(Enum):
     DOUBLE_SHARP = auto()
     SHARP = auto()
     NATURAL = auto()
     FLAT = auto()
     DOUBLE_FLAT = auto()
-
-
-def get_default_accidental(accidental_num, pitch):
-    if accidental_num < 0 and pitch in key_accidental_order[accidental_num:]:
-        return Accidental.FLAT
-    elif accidental_num > 0 and pitch in key_accidental_order[:accidental_num]:
-        return Accidental.SHARP
-    return Accidental.NATURAL
---
+++ src/pitch/Key.py
@@ -0,0 +1,59 @@
+from pitch.Accidental import Accidental
+tonic_order = ["Fb", "Cb", "Gb", "Db", "Ab", "Eb", "Bb",
+               "F", "C", "G", "D", "A", "E", "B",
+               "F#", "C#", "G#", "D#", "A#", "E#", "B#"]
+scale_order = ["Lydian", "Ionian", "Mixolydian",
+               "Dorian", "Aeolian", "Phrygian", "Locrian"]
+key_accidental_order = ["F", "C", "G", "D", "A", "E", "B"]
+
+
+def get_tonic_list(scale_type):
+    start = scale_order.index(scale_type)
+    end = start + 15
+    return tonic_order[start:end]
+
+
+def get_accidental_num(tonic, scale_type):
+    tonic_list = get_tonic_list(scale_type)
+    return tonic_list.index(tonic) - 7
+
+
+class Key:
+    def __init__(self, char):
+        self.char = char
+        self.accidental_num = self._set_accidental()
+
+    def is_major(self):
+        return len(self.char) == 1 or self.char.endswith(("b", "#"))
+
+    def is_minor(self):
+        return self.char.endswith("m")
+
+    def _set_accidental(self):
+        if self.is_major():
+            tonic = self.char
+            scale_type = "Ionian"
+        elif self.is_minor():
+            tonic = self.char[:-1]
+            scale_type = "Aeolian"
+        else:
+            tonic = self.char[:-3]
+            if self.char.endswith("Mix"):
+                scale_type = "Mixolydian"
+            elif self.char.endswith("Dor"):
+                scale_type = "Dorian"
+            elif self.char.endswith("Phr"):
+                scale_type = "Phrygian"
+            elif self.char.endswith("Loc"):
+                scale_type = "Locrian"
+            else:
+                print("error: invalid key string")
+                exit()
+        self.accidental_num = get_accidental_num(tonic, scale_type)
+
+    def get_default_accidental(self, pitch):
+        if self.accidental_num < 0 and pitch in key_accidental_order[self.accidental_num:]:
+            return Accidental.FLAT
+        elif self.accidental_num > 0 and pitch in key_accidental_order[:self.accidental_num]:
+            return Accidental.SHARP
+        return Accidental.NATURAL
